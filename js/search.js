4f3f97fdb0eb91d73dde73d5b1f36b7b20b9349a
Trần Hà Phương <tranhaphuong13@gmail.com>
2018-06-07 22:19:28 +0700

    choice number of result

--- js/search.js
+++ js/search.js
@@ -11,14 +11,15 @@ input.addEventListener("keyup", function(event){
 function wikiopen(){
 	if(document.getElementById("text-search").value !=""){
 	searchBoxValue = document.getElementById("text-search").value;
+	var quantity = document.getElementById("Quantity").value;
 	document.getElementById("contant").innerHTML = "";
-	url = 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&titles=&utf8=1&srsearch=' + searchBoxValue + '&srlimit=15';
+	url = 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&titles=&utf8=1&srsearch=' + searchBoxValue + '&srlimit=' + quantity;
 	// alert(url);
 	$.ajax({
 		url: url,
 		dataType: "jsonp",
 		success: function(rep){
-			for(var i =0; i<15;i++){
+			for(var i =0; i<quantity;i++){
 				$("#contant").append("<div class='box'></div>");
 				/* var cont =*/ $(".box:last").append('<a class="title" target="_blank" href="https://en.wikipedia.org/wiki/' + rep.query.search[i].title + '">' + rep.query.search[i].title + '</a>');
 				//alert(url);
